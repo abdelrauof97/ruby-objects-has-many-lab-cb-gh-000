1f34e61f4efa352a1174649f2202b0046d9f6691
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-18 23:28:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,11 @@ class Song
   end
   
   def artist_name 
-    self.artist_name
-    nil if self.artist.name == " "
+    if self.artist.name == " "
+      nil 
+    else 
+      self.artist.name
+    end
   end
   
 end
